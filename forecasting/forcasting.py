86559817e37e24a52e251b1f988b2431ca54194e
Alina Burykina <alina4268@mail.ru>
2024-07-01 00:56:25 +0300

    cover darts

--- forecasting/forcasting.py
+++ forecasting/forcasting.py
@@ -9,6 +9,10 @@ from darts.models import AutoARIMA
 from darts.models import RNNModel
 from darts.models import TFTModel
 from pytorch_lightning.callbacks import EarlyStopping
+from typing import Tuple
+import pandas as pd
+from darts import TimeSeries
+from darts.dataprocessing.transformers import Scaler
 
 
 my_stopper = EarlyStopping(
@@ -20,12 +24,23 @@ my_stopper = EarlyStopping(
 
 
 class BaseModelHandler(ABC):
-    def __init__(self, file_name, work_dir):
-        self.name = file_name
-        self.work_dir = work_dir
-        self.ensure_directory(self.work_dir)
+    def __init__(self, dataset, 
+                 test_percent, 
+                 forecasting_horizont, 
+                 target_columns,
+                 conditional_columns):
+        
+        self.dataset = dataset
+        self.test_percent = test_percent
+        self.forecasting_horizont = forecasting_horizont
+        self.target_columns = target_columns
+        self.conditional_columns = conditional_columns
+        self.scaler = None
+        self._get_darts_format()
+
         self.model = None
         self.trained = False
+        self.input_chunk_length = 100
 
         self.accelerator_type = "gpu" if torch.cuda.is_available() else "cpu"
         device_count = 1
@@ -35,13 +50,49 @@ class BaseModelHandler(ABC):
             "devices": device_count,
             "callbacks": [my_stopper],
         }
+    
+    def _get_darts_format(self):
+        split_idx = int(len(self.dataset) * (1 - self.test_percent))
+
+        df_train = self.dataset[:split_idx]
+        df_test = self.dataset[split_idx:]
+
+        self.train_ts, self.train_cov = self.process_dataset(df_train)
+        self.test_ts, self.test_cov = self.process_dataset(df_test)
+    
+    def process_dataset(
+        self, df: pd.DataFrame
+    ) -> Tuple[TimeSeries, TimeSeries, Scaler]:
+        """
+        Processes a DataFrame by encoding temporal features, applying scaling, and converting to TimeSeries.
+
+        Parameters:
+        df (pd.DataFrame): DataFrame to process.
+
+        Returns:
+        Tuple[TimeSeries, TimeSeries, Scaler]: Tuple containing the original and the feature-augmented TimeSeries and the scaler used.
+        """
+        ts = TimeSeries.from_dataframe(df[self.target_columns], freq=df.index.freq)
+
+        if self.scaler is None:
+            self.scaler = Scaler()
+            ts = self.scaler.fit_transform(ts)
+        else:
+            ts = self.scaler.transform(ts)
+
+        ts_cov = TimeSeries.from_dataframe(df[self.conditional_columns], freq=df.index.freq)
+
+        return ts, ts_cov
+    
+    def get_true_test(self):
+        return self.test_ts.pd_dataframe()
 
     def ensure_directory(self, path):
         """Ensures that the directory exists."""
         os.makedirs(path, exist_ok=True)
 
     @abstractmethod
-    def train_model(self, train, train_cov, params):
+    def train_model(self, train, train_cov, params): #fit
         """Training method to be implemented by each model type handler."""
         pass
 
@@ -54,19 +105,23 @@ class BaseModelHandler(ABC):
             raise Exception("Model not found. Please train the model first.")
 
     @abstractmethod
-    def predict(self, series, horizon):
+    def predict(self, series, horizon): #forecast
         """Predict method to be implemented by subclasses."""
         return
 
-    def save_model(self):
+    def save_model(self, file_name, work_dir):
         """Save model to file, might need to be overridden depending on model specifics."""
+        self.name = file_name
+        self.work_dir = work_dir
+        self.ensure_directory(self.work_dir)
+
         if self.model:
             model_path = os.path.join(self.work_dir, f"{self.name}.pkl")
             self.model.save(model_path)
         else:
             raise ValueError("No model to save.")
 
-    def backtest(self, Y_train, X_train, Y, X, horizon, scaler):
+    def backtest(self):
         """
         Conducts a backtest by simulating the model's performance on unseen data using a sliding window approach.
 
@@ -98,34 +153,37 @@ class BaseModelHandler(ABC):
             self.load_model()
 
         input_size = self.input_chunk_length
-        Y = Y_train[-input_size:].append(Y)
-        X = X_train[-input_size:].append(X)
+        horizon = self.forecasting_horizont
+
+        Y = self.train_ts[-input_size:].append(self.test_ts)
+        X = self.train_cov[-input_size:].append(self.test_cov)
 
         future_preds = []
         future_targets = []
 
         for i in range(0, len(Y) - input_size - horizon + 1, horizon):
+            print(i)
             past_target = Y[i : i + input_size]
             future_target_original = Y[i + input_size : i + input_size + horizon]
 
             future_exog = X[i : i + input_size + horizon]
             future_pred = self.model.predict(
-                horizon,
+                horizon if horizon <= len(future_exog) else len(future_exog),
                 series=past_target,
                 future_covariates=future_exog,
                 verbose=False,
             )
 
-            if scaler is not None:
-                future_pred_values = scaler.inverse_transform(future_pred).values()
-                future_target_values = scaler.inverse_transform(
+            if self.scaler is not None:
+                future_pred_values = self.scaler.inverse_transform(future_pred).values()
+                future_target_values = self.scaler.inverse_transform(
                     future_target_original
                 ).values()
 
             future_pred = future_pred.values()
             future_target_original = future_target_original.values()
 
-            if scaler is None:
+            if self.scaler is None:
                 future_pred_values = future_pred
                 future_target_values = future_target_original
 
@@ -136,8 +194,8 @@ class BaseModelHandler(ABC):
 
 
 class ARIMAModelHandler(BaseModelHandler):
-    def __init__(self, params=None, work_dir="forcasting_models", name="arima"):
-        super().__init__(name, work_dir)
+    def __init__(self, dataset, test_percent, forecasting_horizont, target_columns, conditional_columns, params=None):
+        super().__init__(dataset, test_percent, forecasting_horizont, target_columns, conditional_columns)
         if params is None:
             params = {
                 "start_p": 0,
@@ -162,28 +220,31 @@ class ARIMAModelHandler(BaseModelHandler):
         self.params = params
         self.model = AutoARIMA(**params)
 
-    def train_model(self, train, train_cov):
-        self.model.fit(train, future_covariates=train_cov)
-        self.save_model()
+    def train_model(self):
+        self.model.fit(self.train_ts, future_covariates=self.train_cov)
         self.trained = True
 
-    def predict(self, test_cov, horizon):
+    def predict(self, all_test=True):
         if not self.trained:
             self.load_model()
-        return self.model.predict(horizon, future_covariates=test_cov, verbose=False)
+        pred_len = len(self.test_cov) if all_test else self.forecasting_horizont
+        return self.model.predict(pred_len, future_covariates=self.test_cov, verbose=False).pd_dataframe()
+    
+    def save_model(self, work_dir="forcasting_models", file_name="arima"):
+        return super().save_model(file_name, work_dir)
 
 
 class LSTMModelHandler(BaseModelHandler):
     def __init__(
         self,
+        dataset, test_percent, forecasting_horizont, target_columns, conditional_columns,
         input_chunk_length=100,
         n_rnn_layers=1,
         n_epochs=20,
         dropout=0.0,
-        work_dir="forcasting_models",
-        name="lstm",
+        
     ):
-        super().__init__(name, work_dir)
+        super().__init__(dataset, test_percent, forecasting_horizont, target_columns, conditional_columns)
         self.input_chunk_length = input_chunk_length
         self.n_rnn_layers = n_rnn_layers
         self.n_epochs = n_epochs
@@ -195,17 +256,17 @@ class LSTMModelHandler(BaseModelHandler):
             dropout=self.dropout,
             n_rnn_layers=self.n_rnn_layers,
             n_epochs=self.n_epochs,
-            work_dir=self.work_dir,
+            # work_dir=self.work_dir,
             save_checkpoints=False,
             random_state=42,
             pl_trainer_kwargs=self.pl_trainer_kwargs,
         )
 
-    def train_model(self, train, train_cov):
+    def train_model(self):
         print(f"Training on {self.accelerator_type.upper()}.")
-        val_size = int(len(train) * 0.1)
-        train_data, val_data = train[:-val_size], train[-val_size:]
-        train_cov_data, val_cov_data = train_cov[:-val_size], train_cov[-val_size:]
+        val_size = int(len(self.train_ts) * 0.1)
+        train_data, val_data = self.train_ts[:-val_size], self.train_ts[-val_size:]
+        train_cov_data, val_cov_data = self.train_cov[:-val_size], self.train_cov[-val_size:]
 
         self.model.fit(
             train_data,
@@ -213,23 +274,27 @@ class LSTMModelHandler(BaseModelHandler):
             val_series=val_data,
             val_future_covariates=val_cov_data,
         )
-        self.save_model()
         self.trained = True
 
-    def predict(self, train, train_cov, test_cov, horizon):
+    def predict(self, all_test=True):
         if not self.trained:
             self.load_model()
+        pred_len = len(self.test_cov) if all_test else self.forecasting_horizont
         return self.model.predict(
-            horizon,
-            series=train,
-            future_covariates=train_cov[-self.input_chunk_length :].append(test_cov),
+            pred_len,
+            series=self.train_ts,
+            future_covariates=self.train_cov[-self.input_chunk_length :].append(self.test_cov),
             verbose=False,
-        )
-
+        ).pd_dataframe()
+    
+    def save_model(self, work_dir="forcasting_models", file_name="lstm"):
+        return super().save_model(file_name, work_dir)
+  
 
 class TFTModelHandler(BaseModelHandler):
     def __init__(
         self,
+        dataset, test_percent, forecasting_horizont, target_columns, conditional_columns,
         input_chunk_length=100,
         forcasting_horizon=24,
         n_epochs=20,
@@ -237,7 +302,7 @@ class TFTModelHandler(BaseModelHandler):
         work_dir="forcasting_models",
         name="tft",
     ):
-        super().__init__(name, work_dir)
+        super().__init__(dataset, test_percent, forecasting_horizont, target_columns, conditional_columns)
         self.input_chunk_length = input_chunk_length
         self.forcasting_horizon = forcasting_horizon
         self.n_epochs = n_epochs
@@ -253,18 +318,18 @@ class TFTModelHandler(BaseModelHandler):
             hidden_continuous_size=8,
             dropout=0.0,
             n_epochs=self.n_epochs,
-            work_dir="tft_new",
+            # work_dir="tft_new",
             save_checkpoints=False,
             pl_trainer_kwargs=self.pl_trainer_kwargs,
             random_state=42,
             batch_size=8,
         )
 
-    def train_model(self, train, train_cov):
+    def train_model(self):
         print(f"Training on {self.accelerator_type.upper()}.")
-        val_size = int(len(train) * 0.1)
-        train_data, val_data = train[:-val_size], train[-val_size:]
-        train_cov_data, val_cov_data = train_cov[:-val_size], train_cov[-val_size:]
+        val_size = int(len(self.train_ts) * 0.1)
+        train_data, val_data = self.train_ts[:-val_size], self.train_ts[-val_size:]
+        train_cov_data, val_cov_data = self.train_cov[:-val_size], self.train_cov[-val_size:]
 
         self.model.fit(
             train_data,
@@ -272,15 +337,18 @@ class TFTModelHandler(BaseModelHandler):
             val_series=val_data,
             val_future_covariates=val_cov_data,
         )
-        self.save_model()
         self.trained = True
 
-    def predict(self, train, train_cov, test_cov, horizon):
+    def predict(self, all_test=True):
         if not self.trained:
             self.load_model()
+        pred_len = len(self.test_cov) if all_test else self.forecasting_horizont
         return self.model.predict(
-            horizon,
-            series=train,
-            future_covariates=train_cov[-self.input_chunk_length :].append(test_cov),
+            pred_len,
+            series=self.train_ts,
+            future_covariates=self.train_cov[-self.input_chunk_length :].append(self.test_cov),
             verbose=False,
-        )
+        ).pd_dataframe()
+    
+    def save_model(self, work_dir="forcasting_models", file_name="tft"):
+        return super().save_model(file_name, work_dir)
--- forecasting/utils/data.py
+++ forecasting/utils/data.py
@@ -1,63 +1,22 @@
 from typing import List
 from typing import Literal
-from typing import Optional
-from typing import Tuple
 
 import numpy as np
 import pandas as pd
-from darts import TimeSeries
-from darts.dataprocessing.transformers import Scaler
-
 
 FreqType = Literal["weekofyear", "hoursofday", "dayofweek"]
 
 
-class TimeSeriesProcessor:
-    """
-    Processes a time series DataFrame to prepare for time series forecasting, including encoding time-based features,
-    scaling the data, and splitting into training and testing sets.
-
-    Attributes:
-    df (pd.DataFrame): The full dataset as a pandas DataFrame.
-    test_size (float): Proportion of the dataset to set aside for testing.
-    freq (str): The frequency of the time series data.
-    freqs (List[FreqType]): List of temporal frequencies to encode as features.
-
-    Methods:
-    positional_encoding: Encodes specified frequencies in the datetime index as sine and cosine features.
-    add_immutable: Adds encoded time features to a DataFrame and converts it to a TimeSeries object.
-    process_dataset: Processes a DataFrame by encoding features, scaling, and converting to TimeSeries.
-    prepare_data: Prepares and splits the data into training and testing sets, suitable for model training.
-    """
-
+class AddImmutable:
     def __init__(
-        self, df: pd.DataFrame, test_size: float, freq: str, freqs: List[FreqType]
+        self, df: pd.DataFrame, freqs: List[FreqType]
     ):
-        """
-        Initializes the TimeSeriesProcessor with the dataset and configuration parameters.
 
-        Parameters:
-        df (pd.DataFrame): Input dataset as a pandas DataFrame.
-        test_size (float): Fraction of the dataset to be used as the test set.
-        freq (str): String representing the frequency of the time series data, e.g., 'D' for daily.
-        freqs (List[FreqType]): List of string identifiers for the datetime components to be encoded.
-        """
         self.df = df
-        self.test_size = test_size
-        self.freq = freq
         self.freqs = freqs
-        self.scaler = None
 
-    def positional_encoding(self, index: pd.DatetimeIndex) -> pd.DataFrame:
-        """
-        Generates sine and cosine transformations of datetime indices for specified frequencies.
-
-        Parameters:
-        index (pd.DatetimeIndex): Datetime index from the DataFrame.
-
-        Returns:
-        pd.DataFrame: DataFrame containing the cosine and sine encoded features.
-        """
+    def positional_encoding(self) -> pd.DataFrame:
+        index = self.df.index
         encoding = []
         for freq in self.freqs:
             if freq == "weekofyear":
@@ -77,63 +36,137 @@ class TimeSeriesProcessor:
 
         return pd.concat(encoding, axis=1)
 
-    def add_immutable(self, df: pd.DataFrame) -> TimeSeries:
-        """
-        Adds encoded time features to the provided DataFrame and returns it as a TimeSeries object.
+    def get(self):
+        additional_columns = self.positional_encoding()
 
-        Parameters:
-        df (pd.DataFrame): DataFrame whose index is used to generate time features.
+        df_concat = pd.concat([self.df, additional_columns], axis=1)
+        df_concat.index.freq = self.df.index.freq
 
-        Returns:
-        TimeSeries: Darts TimeSeries object with additional time features.
-        """
-        additional_columns = self.positional_encoding(df.index)
-        return TimeSeries.from_dataframe(additional_columns, freq=self.freq)
+        return df_concat
+    
 
-    def process_dataset(
-        self, df: pd.DataFrame
-    ) -> Tuple[TimeSeries, TimeSeries, Scaler]:
-        """
-        Processes a DataFrame by encoding temporal features, applying scaling, and converting to TimeSeries.
+# class TimeSeriesProcessor:
+#     """
+#     Processes a time series DataFrame to prepare for time series forecasting, including encoding time-based features,
+#     scaling the data, and splitting into training and testing sets.
 
-        Parameters:
-        df (pd.DataFrame): DataFrame to process.
+#     Attributes:
+#     df (pd.DataFrame): The full dataset as a pandas DataFrame.
+#     test_size (float): Proportion of the dataset to set aside for testing.
+#     freq (str): The frequency of the time series data.
+#     freqs (List[FreqType]): List of temporal frequencies to encode as features.
 
-        Returns:
-        Tuple[TimeSeries, TimeSeries, Scaler]: Tuple containing the original and the feature-augmented TimeSeries and the scaler used.
-        """
-        df.index = pd.to_datetime(df.index)
-        ts = TimeSeries.from_dataframe(df, freq=self.freq)
+#     Methods:
+#     positional_encoding: Encodes specified frequencies in the datetime index as sine and cosine features.
+#     add_immutable: Adds encoded time features to a DataFrame and converts it to a TimeSeries object.
+#     process_dataset: Processes a DataFrame by encoding features, scaling, and converting to TimeSeries.
+#     prepare_data: Prepares and splits the data into training and testing sets, suitable for model training.
+#     """
 
-        if self.scaler is None:
-            self.scaler = Scaler()
-            ts = self.scaler.fit_transform(ts)
-        else:
-            ts = self.scaler.transform(ts)
+#     def __init__(
+#         self, df: pd.DataFrame, test_size: float, freq: str, freqs: List[FreqType]
+#     ):
+#         """
+#         Initializes the TimeSeriesProcessor with the dataset and configuration parameters.
 
-        ts_with_features = self.add_immutable(df)
+#         Parameters:
+#         df (pd.DataFrame): Input dataset as a pandas DataFrame.
+#         test_size (float): Fraction of the dataset to be used as the test set.
+#         freq (str): String representing the frequency of the time series data, e.g., 'D' for daily.
+#         freqs (List[FreqType]): List of string identifiers for the datetime components to be encoded.
+#         """
+#         self.df = df
+#         self.test_size = test_size
+#         self.freq = freq
+#         self.freqs = freqs
+#         self.scaler = None
 
-        return ts, ts_with_features
+#     def positional_encoding(self, index: pd.DatetimeIndex) -> pd.DataFrame:
+#         """
+#         Generates sine and cosine transformations of datetime indices for specified frequencies.
 
-    def prepare_data(
-        self, forecast_horizon: int
-    ) -> Tuple[TimeSeries, TimeSeries, TimeSeries, TimeSeries, int, Scaler]:
-        """
-        Prepares data for training and testing, splits the dataset, and returns the necessary components for forecasting.
+#         Parameters:
+#         index (pd.DatetimeIndex): Datetime index from the DataFrame.
 
-        Parameters:
-        forecast_horizon (int): The number of periods ahead to forecast.
+#         Returns:
+#         pd.DataFrame: DataFrame containing the cosine and sine encoded features.
+#         """
+#         encoding = []
+#         for freq in self.freqs:
+#             if freq == "weekofyear":
+#                 values = index.isocalendar().week
+#             elif freq == "hoursofday":
+#                 values = index.hour
+#             elif freq == "dayofweek":
+#                 values = index.dayofweek
+#             num_values = max(values) + 1
+#             steps = [x * 2.0 * np.pi / num_values for x in values]
+#             encoding.append(
+#                 pd.DataFrame(
+#                     {f"{freq}_cos": np.cos(steps), f"{freq}_sin": np.sin(steps)},
+#                     index=index,
+#                 )
+#             )
 
-        Returns:
-        Tuple[TimeSeries, TimeSeries, TimeSeries, TimeSeries, int, Scaler]: Tuple containing the training and testing time series,
-        both raw and with covariates, the forecast horizon, and the scaler used.
-        """
-        split_idx = int(len(self.df) * (1 - self.test_size))
+#         return pd.concat(encoding, axis=1)
 
-        df_train = self.df[:split_idx]
-        df_test = self.df[split_idx:]
+#     def add_immutable(self, df: pd.DataFrame) -> TimeSeries:
+#         """
+#         Adds encoded time features to the provided DataFrame and returns it as a TimeSeries object.
 
-        train_ts, train_cov = self.process_dataset(df_train)
-        test_ts, test_cov = self.process_dataset(df_test)
+#         Parameters:
+#         df (pd.DataFrame): DataFrame whose index is used to generate time features.
 
-        return train_ts, train_cov, test_ts, test_cov, forecast_horizon, self.scaler
+#         Returns:
+#         TimeSeries: Darts TimeSeries object with additional time features.
+#         """
+#         additional_columns = self.positional_encoding(df.index)
+#         return TimeSeries.from_dataframe(additional_columns, freq=self.freq)
+
+#     def process_dataset(
+#         self, df: pd.DataFrame
+#     ) -> Tuple[TimeSeries, TimeSeries, Scaler]:
+#         """
+#         Processes a DataFrame by encoding temporal features, applying scaling, and converting to TimeSeries.
+
+#         Parameters:
+#         df (pd.DataFrame): DataFrame to process.
+
+#         Returns:
+#         Tuple[TimeSeries, TimeSeries, Scaler]: Tuple containing the original and the feature-augmented TimeSeries and the scaler used.
+#         """
+#         df.index = pd.to_datetime(df.index)
+#         ts = TimeSeries.from_dataframe(df, freq=self.freq)
+
+#         if self.scaler is None:
+#             self.scaler = Scaler()
+#             ts = self.scaler.fit_transform(ts)
+#         else:
+#             ts = self.scaler.transform(ts)
+
+#         ts_with_features = self.add_immutable(df)
+
+#         return ts, ts_with_features
+
+#     def prepare_data(
+#         self, forecast_horizon: int
+#     ) -> Tuple[TimeSeries, TimeSeries, TimeSeries, TimeSeries, int, Scaler]:
+#         """
+#         Prepares data for training and testing, splits the dataset, and returns the necessary components for forecasting.
+
+#         Parameters:
+#         forecast_horizon (int): The number of periods ahead to forecast.
+
+#         Returns:
+#         Tuple[TimeSeries, TimeSeries, TimeSeries, TimeSeries, int, Scaler]: Tuple containing the training and testing time series,
+#         both raw and with covariates, the forecast horizon, and the scaler used.
+#         """
+#         split_idx = int(len(self.df) * (1 - self.test_size))
+
+#         df_train = self.df[:split_idx]
+#         df_test = self.df[split_idx:]
+
+#         train_ts, train_cov = self.process_dataset(df_train)
+#         test_ts, test_cov = self.process_dataset(df_test)
+
+#         return train_ts, train_cov, test_ts, test_cov, forecast_horizon, self.scaler
