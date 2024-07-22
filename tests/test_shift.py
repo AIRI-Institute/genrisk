ce94c4201a323baadfebbd86ff0205aeb91f00ef
Alina Burykina <alina4268@mail.ru>
2024-07-22 19:03:34 +0300

    return utils

--- tests/test_shift.py
+++ tests/test_shift.py
@@ -1,18 +1,12 @@
-import numpy as np
 import pandas as pd
-
-from genrisk.shift_old import ConditionalShift
-
+import numpy as np
+from genrisk.shift import ConditionalShift
 
 def test_conditional_shift():
-    shift_model = ConditionalShift(mutable_columns=["X1"], immutable_columns=["X2"])
+    shift_model = ConditionalShift(mutable_columns=['X1'], immutable_columns=['X2'])
     X = pd.DataFrame(
         np.random.randn(1000, 2),
-        columns=["X1", "X2"],
+        columns=['X1', 'X2'],
     )
     error = pd.Series(np.random.randn(1000))
     shift_model.fit(X, error)
-    assert len(shift_model.mask) == len(X)
-    assert shift_model.risk
-    assert shift_model.ub_risk
-    assert shift_model.lb_risk
