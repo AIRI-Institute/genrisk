8e795cefeb653fa18ab1b50c64af8c2491db1447
alina <alina4268@mail.ru>
2024-08-18 00:04:30 +0300

    add new way for wcr

--- genrisk/tests/test_shift.py
+++ genrisk/tests/test_shift.py
@@ -2,15 +2,21 @@ import pandas as pd
 import numpy as np
 from genrisk.shift import ConditionalShift
 
+
 def test_conditional_shift():
-    shift_model = ConditionalShift(mutable_columns=['X1'], immutable_columns=['X2'])
+    shift_model = ConditionalShift(
+        mutable_columns=['X1'],
+        immutable_columns=['X2'],
+        alpha_space=list(np.linspace(0.05, 0.95, 10)),
+    )
     X = pd.DataFrame(
         np.random.randn(1000, 2),
         columns=['X1', 'X2'],
     )
     error = pd.Series(np.random.randn(1000))
-    shift_model.fit(X, error)
+    output = shift_model.fit(X, error)
     assert len(shift_model.mask) == len(X)
-    assert shift_model.risk
-    assert shift_model.ub_risk
-    assert shift_model.lb_risk
+    assert len(output) == len(list(np.linspace(0.05, 0.95, 10)))
+    assert output[0]
+    assert output[1]
+    assert output[2]
